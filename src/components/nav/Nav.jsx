cb7354d2fb6732897e4b268a6761809bf6c5d90f
Marwan-Mokhtar <marwanmokhtar932@gmail.com>
2025-08-26 12:02:10 +0300

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,58 +6,39 @@ import { BiBook } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDetail } from "react-icons/bi";
 const Nav = () => {
+  const iconData = [
+    { id: 1, href: "#", svg: <IoHomeOutline /> },
+    { id: 2, href: "#about", svg: <LuUserRound /> },
+    { id: 3, href: "#services", svg: <BiBook /> },
+    {
+      id: 4,
+      href: "#projects",
+      svg: <RiServiceLine />,
+    },
+    { id: 5, href: "#contact", svg: <BiMessageSquareDetail /> },
+  ];
   const [activeNav, SetActiveNav] = useState("#");
   return (
     <div className="nav">
       <ul>
-        <li>
-          <a
-            href="#"
-            onClick={() => SetActiveNav("#")}
-            className={activeNav === "#" ? "active" : ""}
-          >
-            <IoHomeOutline />
-          </a>
-        </li>
-        <li>
-          <a
-            href="#about"
-            onClick={() => SetActiveNav("#about")}
-            className={activeNav === "#about" ? "active" : ""}
-          >
-            <LuUserRound />
-          </a>
-        </li>
-        <li>
-          <a
-            href="#services"
-            onClick={() => SetActiveNav("#services")}
-            className={activeNav === "#services" ? "active" : ""}
-          >
-            <BiBook />
-          </a>
-        </li>
-        <li>
-          <a
-            href="#projects"
-            onClick={() => SetActiveNav("#projects")}
-            className={activeNav === "#projects" ? "active" : ""}
-          >
-            <RiServiceLine />
-          </a>
-        </li>
-        <li>
-          <a
-            href="#contact"
-            onClick={() => SetActiveNav("#contact")}
-            className={activeNav === "#contact" ? "active" : ""}
-          >
-            <BiMessageSquareDetail />
-          </a>
-        </li>
+        {iconData.map((icon) => {
+          return (
+            <li>
+              <a
+                key={icon.id}
+                href={icon.href}
+                className={activeNav === icon.href ? "active" : ""}
+                onClick={() => SetActiveNav(icon.href)}
+              >
+                {icon.svg}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
 };
 
 export default Nav;
+
